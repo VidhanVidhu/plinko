3362be230823f7ee04c331e485053055e50a228e
VidhanVidhu <72401049+VidhanVidhu@users.noreply.github.com>
2020-11-30 20:06:33 +0530

    Add files via upload

--- ground.js
+++ ground.js
@@ -1,10 +1,10 @@
-class Ground {
+class Ground{
     constructor(x,y,w,h) {
       var options = {
           isStatic: true
       }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.w = w;
+      this.body = Bodies.rectangle(x,y,w,h,options);
+      this.w = w; 
       this.h = h;
       World.add(world, this.body);
     }
@@ -12,6 +12,6 @@ class Ground {
       var pos =this.body.position;
       rectMode(CENTER);
       fill("white");
-      rect(pos.x, pos.y, this.w, this.h);
+    rect(pos.x, pos.y, this.w,this.h);
     }
   };
